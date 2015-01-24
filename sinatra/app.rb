2d29328dfa5fe0af2f0bc1d99d7f2a9c41e34b96
Nelson Taveras <ntaveras@okkralabs.com>
2015-01-24 15:50:55 -0400

    Added author route

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -12,3 +12,9 @@ get '/posts/:id' do
   @posts = blog.find_by_id(id)
   erb :index
 end
+
+get '/author/:author' do
+  author = params[:author].to_str
+  @posts = blog.find_by_author(author)
+  erb :index
+end
