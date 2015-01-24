6da02aee428163edf180958ccd6a1d619a7bbd51
Nelson Taveras <ntaveras@okkralabs.com>
2015-01-24 04:09:01 -0400

    Added posts route

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require_relative '../lib/blog.rb'
+
+blog = Blog.new
 
 get '/' do
+  erb :index
+end
+
+get '/posts/:id' do
+  id = params[:id].to_i
+  @posts = blog.find_by_id(id)
   erb :index
 end
