e7ff61bd4ad55606bf2eb4b79cb56bc63d934c96
Nelson Taveras <ntaveras@okkralabs.com>
2015-01-24 00:50:54 -0400

    Small tests refactoring

--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -10,6 +10,11 @@ describe Blog do
   describe "#initialize" do
     it "Reads the posts.json file and returns a Blog object" do
       @blog.class.should == Blog
+    end
+  end
+
+  describe "#posts" do
+    it "Should countain all the posts in the JSON file" do
       @blog.posts.count.should == 3
     end
   end
