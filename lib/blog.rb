9d74cfec0808f8313ec5a64a38e4b2d435cfc6d7
Nelson Taveras <ntaveras@okkralabs.com>
2015-01-24 00:47:29 -0400

    Added initialize method

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,3 +1,12 @@
+require 'json'
+
 class Blog
-  
+
+  attr_accessor :posts
+
+  def initialize
+    posts_file = File.read('posts.json')
+    @posts = JSON.parse(posts_file)['posts']
+  end
+
 end
--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 require 'blog'
  
 describe Blog do
+
+  before :each do
+    @blog = Blog.new
+  end
+
+  describe "#initialize" do
+    it "Reads the posts.json file and returns a Blog object" do
+      @blog.class.should == Blog
+      @blog.posts.count.should == 3
+    end
+  end
+
 end
